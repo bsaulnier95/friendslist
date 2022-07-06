f048759d7f0da7d6a7749436d52dcfbcb452dcaf
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-05 22:16:16 -0400

    hit

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import '../js/bootstrap_js_files.js'  
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
