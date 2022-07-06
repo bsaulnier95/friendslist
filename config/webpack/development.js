a83a7cfa081fd607d0fcd11f2977bf8c78d29da1
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-06 13:13:33 -0400

    ugh

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,9 +2,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-module.exports =  environment.toWebpackConfig()
-{
-  resolve: {
-    extensions: ['.js', '.jsx']  //Working
-  }
-};
