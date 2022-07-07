95d04963533a8af472071e242724691e54d2f17b
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-07 14:29:09 -0400

    hi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,14 +14,3 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-babelHelpers.toConsumableArray = function (arr) {
-  if (Array.isArray(arr)) {
-    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
-      arr2[i] = arr[i];
-    }
-
-    return arr2;
-  } else {
-    return Array.from(arr);
-  }
-}
