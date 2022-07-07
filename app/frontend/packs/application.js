6ce960f66e925a680fce73012e3bb95ea7463908
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-07 16:16:21 -0400

    woo

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 import '../js/bootstrap_js_files.js'  
 import 'core-js/stable'
+import 'core-js/es/array'
 import 'regenerator-runtime/runtime'
 require("@rails/ujs").start()
 require("turbolinks").start()
