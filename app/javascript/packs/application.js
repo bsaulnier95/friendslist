ccde3a5c4f88a9a2555b26dee14e7604b73a61bd
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-07-06 22:20:19 -0400

    why

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import Rails from "@rails/ujs"
+import "jquery"
+import {createPopper} from '@popperjs/core'
